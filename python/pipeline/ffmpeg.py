27d442b61fbedfeda0dd29537c6cb327dbe2661a
Mathias Claeys <mathiasclaeys@hotmail.com>
2014-10-16 15:50:41 +0200

    hide the popup windows update

--- python/pipeline/ffmpeg.py
+++ python/pipeline/ffmpeg.py
@@ -1,10 +1,12 @@
 import os, subprocess
 
+CREATE_NO_WINDOW  = 0x00000008
+
 def test():
 	print "Succesfull test!"
 
 # ffmpegPath = r"W:/WG/WTD_Code/trunk/wtd/pipeline/resources/ffmpeg/bin/ffmpeg.exe" 
-def ffmpegMakingSlates(inputFilePath, outputFilePath, audioPath = "", topleft = "", topmiddle = "", topright = "", bottomleft = "", bottommiddle = "", bottomright = "", ffmpegPath = "ffmpeg.exe", font = "arial.ttf", font_size = 16, font_color = "gray", slate_height = 21, slate_color = "black@0.8", overwrite = True, logLevel = "quiet", endcommand="exit"):
+def ffmpegMakingSlates(inputFilePath, outputFilePath, audioPath = "", topleft = "", topmiddle = "", topright = "", bottomleft = "", bottommiddle = "", bottomright = "", ffmpegPath = "ffmpeg.exe", font = "arial.ttf", font_size = 16, font_color = "white", slate_height = 21, slate_color = "black@1.0", overwrite = True, logLevel = "quiet"):
 	
 	top = "%s/5.0" %slate_height
 	bottom = "h-(%s-%s/5.0-1)" %(slate_height, slate_height)
@@ -22,14 +24,14 @@ def ffmpegMakingSlates(inputFilePath, outputFilePath, audioPath = "", topleft = 
 	drawtext=fontsize={font_size}:fontfile={font}:text={bottomleft}: x={left}: y={bottom}: fontcolor={font_color},\
 	drawtext=fontsize={font_size}:fontfile={font}:text={bottommiddle}: x={middle}: y={bottom}: fontcolor={font_color},\
 	drawtext=fontsize={font_size}:fontfile={font}:text={bottomright}: x={right}: y={bottom}: fontcolor={font_color}"\
-	"{output}" {overwrite} {endcommand}'.format(
+	"{output}" {overwrite}'.format(
 		ffmpeg=ffmpegPath, input=inputFilePath, output=outputFilePath, 
 		topleft=topleft, topmiddle=topmiddle, topright=topright ,bottomleft=bottomleft, bottommiddle=bottommiddle, bottomright=bottomright, 
 		left= "5",middle= "(w-tw)/2", right= "(w-tw)-5", top="%s/5.0" %slate_height, bottom="h-(%s-%s/5.0-1)" %(slate_height, slate_height), 
 		font=font, font_size=font_size, font_color=font_color, 
 		slate_height=slate_height, slate_color=slate_color, overwrite = overwrite, logLevel = logLevel
 		)
-	value = subprocess.call(command_line_arguments)
+	value = subprocess.call(command_line_arguments, creationflags=CREATE_NO_WINDOW, shell=False)
 	return value
 	
 def ffmpegMakingMovie(inputFilePath, outputFilePath, audioPath = "",start_frame = 0, framerate = 24, encodeOptions = None, ffmpegPath = "ffmpeg.exe"):
@@ -45,7 +47,7 @@ def ffmpegMakingMovie(inputFilePath, outputFilePath, audioPath = "",start_frame 
 	if audioPath != "":
 		audio = " -i %s" %audioPath
 		
-	value = subprocess.call('{ffmpeg} -start_number "{start_frame}" -i "{input}"{audio}{codec} -r {framerate} "{output}" -y'.format(ffmpeg=ffmpegPath, input=inputFilePath, output=outputFilePath, codec=codec, audio=audio, start_frame=start_frame, framerate=framerate))
+	value = subprocess.call('{ffmpeg} -start_number "{start_frame}" -i "{input}"{audio}{codec} -r {framerate} "{output}" -y'.format(ffmpeg=ffmpegPath, input=inputFilePath, output=outputFilePath, codec=codec, audio=audio, start_frame=start_frame, framerate=framerate), creationflags=CREATE_NO_WINDOW, shell=False)
 	return value
 	
 def ffmpegConcatFiles(inputDict, outputFilePath, audioDict, ffmpegPath = "ffmpeg.exe"):
@@ -58,4 +60,5 @@ def ffmpegConcatFiles(inputDict, outputFilePath, audioDict, ffmpegPath = "ffmpeg
 	for a in audioDict:
 		concatAudioString += "%s|" %(a)
 	
-	os.system('{ffmpeg} -start_number "{start_frame}" -i "{input}"{audio} "{output}" -y'.format(ffmpeg=ffmpegPath, input=concatString, output=outputFilePath, audio=concatAudioString))
+	value = subprocess.call('{ffmpeg} -start_number "{start_frame}" -i "{input}"{audio} "{output}" -y'.format(ffmpeg=ffmpegPath, input=concatString, output=outputFilePath, audio=concatAudioString))
+	return value
